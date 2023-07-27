666869f653577700ed7a6d057f3e667eeb31c907
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-07-27 18:31:31 +0200

    my first dynamic and reusable react component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,17 @@
-const ExpenseItem = () => {
+import styles from './ExpenseItem.module.css'
+
+const ExpenseItem = (props) => {
+
     return (
-        <>
-            <h2>ExpenseItem</h2>
-        </>
+        <div className={styles['expense-item']}> 
+            <div>{props.expense.date.toLocaleDateString()}</div>
+            <div className={styles['expense-item__description']}> 
+                <h2>{props.expense.title}</h2>
+                <div className={styles['expense-item__price']}>${props.expense.amount}</div>
+            </div>
+            {/* <button onClick={handleNewExpenseDate}>new</button> */}
+        </div>
     )
 }
 
-export default ExpenseItem
+export default ExpenseItem;
