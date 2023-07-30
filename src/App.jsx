a34be9b73c74e98a2fb5c96b6ebe33245a7b699c
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-07-31 00:54:23 +0200

    expenses array without expenses check

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,42 @@ import ExpenseCard from "./components/ExpenseCard";
 
 const App = () => {
   const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 9 - 1, 14),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2 - 1, 12)
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2 - 1, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5 - 1, 12),
-    },
-    {
-      id: 'e5',
-      title: 'Samsung TV',
-      amount: 980,
-      date: new Date(2022, 3 - 1, 17),
-    },
-    {
-      id: 'e6',
-      title: 'iPhone 13',
-      amount: 1250,
-      date: new Date(2023, 6 - 1, 7),
-    },
+    // {
+    //   id: 'e1',
+    //   title: 'Toilet Paper',
+    //   amount: 94.12,
+    //   date: new Date(2020, 9 - 1, 14),
+    // },
+    // {
+    //   id: 'e2',
+    //   title: 'New TV',
+    //   amount: 799.49,
+    //   date: new Date(2021, 2 - 1, 12)
+    // },
+    // {
+    //   id: 'e3',
+    //   title: 'Car Insurance',
+    //   amount: 294.67,
+    //   date: new Date(2021, 2 - 1, 28),
+    // },
+    // {
+    //   id: 'e4',
+    //   title: 'New Desk (Wooden)',
+    //   amount: 450,
+    //   date: new Date(2021, 5 - 1, 12),
+    // },
+    // {
+    //   id: 'e5',
+    //   title: 'Samsung TV',
+    //   amount: 980,
+    //   date: new Date(2022, 3 - 1, 17),
+    // },
+    // {
+    //   id: 'e6',
+    //   title: 'iPhone 13',
+    //   amount: 1250,
+    //   date: new Date(2023, 6 - 1, 7),
+    // },
   ];
 
   return (
--- src/components/ExpenseCard.jsx
+++ src/components/ExpenseCard.jsx
@@ -3,6 +3,15 @@ import styles from './ExpenseCard.module.css'
 
 const ExpenseCard = (props) => {
     const expensesCards = props.expensesArr.map((expense) => <ExpenseItem key={expense.id} expense={expense} />);
+    if (expensesCards.length === 0) {
+        return (
+            <>
+                <div className={styles['expenses']}>
+                    <h2 className={styles['expenses__title']}>No expenses found.</h2>
+                </div>
+            </>
+        )
+    }
     return (
         <>
             <div className={styles['expenses']}>
