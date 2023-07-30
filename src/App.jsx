e8ad992fc090b7be512c7285d268ce4694464101
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-07-30 18:55:00 +0200

    task dates format as calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import ExpenseItem from "./components/ExpenseItem"
 
 const App = () => {
-  // const name = "Tesla";
   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2020, 9, 14),
     },
-    { 
-      id: 'e2', 
-      title: 'New TV', 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12) 
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
     },
     {
       id: 'e3',
@@ -27,15 +26,28 @@ const App = () => {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: 'e5',
+      title: 'Samsung TV',
+      amount: 980,
+      date: new Date(2022, 3, 17),
+    },
+    {
+      id: 'e6',
+      title: 'iPhone 13',
+      amount: 1250,
+      date: new Date(2023, 6, 7),
+    },
   ];
+
+  const expensesCards = expenses.map((expense) => <ExpenseItem key={expense.id} expense={expense} />);
 
   return (
     <>
       <h2>Lets get started!</h2>
-      {/* <ExpenseItem text={name}/> */}
-      {/* <ExpenseItem expense={[...expenses]} /> */}
-
-      {expenses.map((item) => <ExpenseItem key={item.id} expense={item}/>)}
+      <div className="expense-cards">
+        {expensesCards}
+      </div>
     </>
   )
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,15 +1,38 @@
 import styles from './ExpenseItem.module.css'
 
 const ExpenseItem = (props) => {
+    const data = props.expense;
+    const dateObj = new Date(data.date);
+    const year = dateObj.getFullYear();
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth() - 1;
+
+    const monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
 
     return (
-        <div className={styles['expense-item']}> 
-            <div>{props.expense.date.toLocaleDateString()}</div>
-            <div className={styles['expense-item__description']}> 
-                <h2>{props.expense.title}</h2>
-                <div className={styles['expense-item__price']}>${props.expense.amount}</div>
+        <div className={styles['expense-item']}>
+            <div className={styles['calendarDate']}>
+                <div className={styles['month']}>{monthNames[month]}</div>
+                <div className={styles['year']}>{year}</div>
+                <div className={styles['day']}>{day}</div>
             </div>
-            {/* <button onClick={handleNewExpenseDate}>new</button> */}
+            <div className={styles['expense-item__description']}>
+                <h2>{data.title}</h2>
+                <div className={styles['expense-item__price']}>${data.amount}</div>
+            </div>
         </div>
     )
 }
