d0cf476b6783c71ef9cf3418e2f8444ed765bf5f
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-07-31 23:15:46 +0200

    folders restructured

--- src/components/Expenses/ExpenseCard.jsx
+++ src/components/Expenses/ExpenseCard.jsx
@@ -1,22 +1,23 @@
 import ExpenseItem from './ExpenseItem';
 import styles from './ExpenseCard.module.css'
+import Card from '../UI/Card'
 
 const ExpenseCard = (props) => {
     const expensesCards = props.expensesArr.map((expense) => <ExpenseItem key={expense.id} expense={expense} />);
     if (expensesCards.length === 0) {
         return (
             <>
-                <div className={styles['expenses']}>
+                <Card className={styles['expenses']}>
                     <h2 className={styles['expenses__title']}>No expenses found.</h2>
-                </div>
+                </Card>
             </>
         )
     }
     return (
         <>
-            <div className={styles['expenses']}>
+            <Card className={styles['expenses']}>
                 {expensesCards}
-            </div>
+            </Card>
         </>
     )
 }
--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,18 +1,19 @@
 import styles from './ExpenseItem.module.css'
 import ExpenseDate from './ExpenseDate'
+import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
     const expenseData = props.expense;
     const expenseDate = props.expense.date;
 
     return (
-        <div className={styles['expense-item']}>
+        <Card className={styles['expense-item']}>
             <ExpenseDate dates={expenseDate}/>
             <div className={styles['expense-item__description']}>
                 <h2>{expenseData.title}</h2>
                 <div className={styles['expense-item__price']}>${expenseData.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
