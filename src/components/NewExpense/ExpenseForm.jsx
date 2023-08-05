413ad022fb99d56ec441a1644701bd666049a34d
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-08-05 16:33:39 +0200

    Errors handling on form

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import styles from './ExpenseForm.module.css'
+import React, { useState } from 'react';
+import styles from './ExpenseForm.module.css';
 
 const ExpenseForm = () => {
     const [userInput, setUserInput] = useState(() => {
@@ -7,103 +7,77 @@ const ExpenseForm = () => {
             enteredTitle: '',
             enteredDate: '',
             enteredAmount: ''
-        }
+        };
     });
-
-    // const titleChengeHandler = (event) => {
-    //     setUserInput((prevState) => {
-    //         return {
-    //             ...prevState,
-    //             enteredTitle: event.target.value,
-    //         }
-    //     })
-    // };
-
-    // const expenseDateHandler = (event) => {
-    //     setUserInput((prevState) => { 
-    //         return {
-    //             ...prevState,
-    //             enteredDate: event.target.value,
-    //         }
-    //     })
-    // };
-
-    // const expenseAmountHandler = (event) => {
-    //     const value = event.target.value;
-    //     if (!isNaN(value)) {
-    //         setUserInput((prevState) => {
-    //             return {
-    //                 ...prevState,
-    //                 enteredAmount: Number(value),
-    //             }
-    //         })
-    //     } else {
-    //         throw new Error('Input must be of type number')
-    //     }
-    // };
+    
+const [titleError, setTitleError] = useState(false);
 
     const inputHandler = (id, value) => {
-        switch(id){
-            case "title":
-                return setUserInput((prevState) => ({...prevState, enteredTitle: value}));
-            case "date":
-                return setUserInput((prevState) => ({...prevState, enteredDate: value}));
-            case "amount":
-                return setUserInput((prevState) => ({...prevState, enteredAmount: value}));
+        switch (id) {
+            case 'title':
+                setUserInput((prevState) => ({ ...prevState, enteredTitle: value }));
+                setTitleError(value.length < 3); // Set titleError to true if the title length is less than 3
+                break;
+            case 'date':
+                setUserInput((prevState) => ({ ...prevState, enteredDate: value }));
+                break;
+            case 'amount':
+                setUserInput((prevState) => ({ ...prevState, enteredAmount: value }));
+                break;
             default:
-                return null;
+                break;
         }
-    }
+    };
 
     return (
         <form>
-            <div className={styles["new-expense__controls"]}>
-                <div className={styles["new-expense__control"]}>
+            <div className={styles['new-expense__controls']}>
+                <div className={styles['new-expense__control']}>
                     <label htmlFor='expense-title'>Title</label>
+                    
                     <input
-                        type="text"
-                        name="expense-title"
-                        id="expense-title"
-                        placeholder="(E.g: Pizza)"
+                        className={titleError ? styles['title-alert'] : ''}
+                        type='text'
+                        name='expense-title'
+                        id='expense-title'
+                        placeholder='(E.g: Pizza)'
                         value={userInput.enteredTitle}
-                        // onChange={titleChengeHandler}
-                        onChange={(e) => inputHandler("title", e.target.value)}
+                        onChange={(e) => inputHandler('title', e.target.value)}
                     />
+                    {titleError && <p className={styles['error-message']}>Title must have at least three characters</p>}
                 </div>
-                <div className={styles["new-expense__control"]}>
+                <div className={styles['new-expense__control']}>
                     <label htmlFor='expense-date'>Date</label>
                     <input
-                        type="date"
-                        name="expense-date"
-                        id="expense-date"
-                        placeholder=""
-                        min="2023-01-01"
-                        max="2026-12-31"
+                        type='date'
+                        name='expense-date'
+                        id='expense-date'
+                        placeholder=''
+                        min='2023-01-01'
+                        max='2026-12-31'
                         value={userInput.enteredDate}
-                        // onChange={expenseDateHandler}
-                        onChange={(e) => inputHandler("date", e.target.value)}
+                        onChange={(e) => inputHandler('date', e.target.value)}
                     />
                 </div>
-                <div className={styles["new-expense__control"]}>
+                <div className={styles['new-expense__control']}>
                     <label htmlFor='expense-amount'>Amount</label>
                     <input
-                        type="number"
-                        name="expense-amount"
-                        id="expense-amount"
-                        placeholder="Amount"
-                        min="0.01"
-                        step="0.01"
+                        type='number'
+                        name='expense-amount'
+                        id='expense-amount'
+                        placeholder='Amount'
+                        min='0.01'
+                        step='0.01'
                         value={userInput.enteredAmount}
-                        // onChange={expenseAmountHandler}
-                        onChange={(e) => inputHandler("amount", e.target.value)}
+                        onChange={(e) => inputHandler('amount', e.target.value)}
                     />
                 </div>
             </div>
-            <div className={styles["new-expense__actions"]}>
-                <button type="submit">Create</button>
+            <div className={styles['new-expense__actions']}>
+                <button type='submit'>Create</button>
             </div>
         </form>
-    )
-}
+    );
+};
 
-export default ExpenseForm
+export default ExpenseForm;
